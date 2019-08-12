f09126dddc1ed4f65458665f6aeef1ff4e2045fb
daisuke <dsaito85a@gmail.com>
2019-08-12 17:49:21 +0900

    RouterTestからRouterのrequireが成功した

--- phpUnit/RouterTest.php
+++ phpUnit/RouterTest.php
@@ -1,4 +1,8 @@
 <?php
+
+require __DIR__ . '/../core/Router.php';
+
+use core\Router;
 use PHPUnit\Framework\TestCase;
 
 class RouterTest extends TestCase
@@ -14,5 +18,25 @@ class RouterTest extends TestCase
 
         $this->assertSame('foo', array_pop($stack));
         $this->assertSame(0, count($stack));
+    }
+
+    public function testRouterResolve()
+    {
+        $router = new \Core\Router(
+            [
+                '/' => ['controller' => 'home', 'action' => 'welcome'],
+                '/login' => ['controller' => 'login', 'action' => 'login'],
+                '/logout' => ['controller' => 'login', 'action' => 'logout'],
+                '/signup' => ['controller' => 'login', 'action' => 'signupView'],
+                '/register' => ['controller' => 'login', 'action' => 'register'],
+                '/post/create' => ['controller' => 'post', 'action' => 'create'],
+                '/post/:id/delete' => ['controller' => 'post', 'action' => 'delete'],
+                '/post/:id' => ['controller' => 'post', 'action' => 'show'],
+                '/page/:page' => ['controller' => 'post', 'action' => 'showPage'],
+            ]
+        );
+
+        $router->resolve("/");
+
     }
 }
